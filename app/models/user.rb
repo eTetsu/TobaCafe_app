e8ae168c40352677252b85f444010149a2ae0b67
遠藤鉄太 <e.t.@MacBookAir.local>
2026-01-12 15:09:25 +0900

    Add: ユーザー登録機能の実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,7 @@ class User < ApplicationRecord
   validates :password, length: { minimum: 3 }, if: -> { new_record?  || chenges[:crypted_password] }
   validates :password, confirmation: true, if: -> { new_record? || changes[:crypted_password] }
   validates :password_confirmation, presence: true, if: -> { new_record? || changes[:crypted_password] }
-
   validates :nickname, presence: true, length: { maximum: 255 }
   validates :email, presence: true, uniqueness: true, length: { maximum: 255 }
+  validates :email, format: { with: URI::MailTo::EMAIL_REGEXP }, allow_blank: true
 end
